4abd4338b2c0f608d6e53b2219bec81250293b59
Asim Mahmood <asim1999@hotmail.co.uk>
2023-06-07 13:51:03 +0100

    Added starter code for trip_provider.dart

---
+++ lib/provider/trip_provider.dart
@@ -0,0 +1,25 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
+import 'package:uuid/uuid.dart';
+
+import 'models/trip.dart';
+import 'package:http/http.dart' as http;
+
+class TripProvider extends ChangeNotifier{
+//  Variables
+//ID generator
+final _uuid = const Uuid();
+//  Trips list
+List<Trip> _trips = [];
+// Methods
+//Trips
+//Get trip
+//Add trip
+//Delete trip
+
+//Trip activities
+//Get trip activities
+//Add trip activity
+//Delete trip activity
+}
